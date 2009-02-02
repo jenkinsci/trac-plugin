22710f655f08acf52d1f43db2e7f64d35f412d44
bradfritz <bradfritz@dev.java.net>
2009-02-02 03:47:33 +0000

    [HUDSON-2707] Second part of Rick Riemer's patch.  Additional tests for annotations.

--- src/test/java/hudson/plugins/trac/TracLinkAnnotatorTest.java
+++ src/test/java/hudson/plugins/trac/TracLinkAnnotatorTest.java
@@ -14,6 +14,19 @@ public class TracLinkAnnotatorTest extends TestCase {
         assertAnnotatedTextEquals("Text with WikiLink.", "Text with <a href='" + TRAC_URL + "wiki/WikiLink'>WikiLink</a>.");
     }
 
+    public void testTicketLinkSyntax() {
+        assertAnnotatedTextEquals("Text with ticket #123 link",
+                "Text with ticket <a href='" + TRAC_URL + "ticket/123'>#123</a> link");
+        assertAnnotatedTextEquals("#123",
+                "<a href='" + TRAC_URL + "ticket/123'>#123</a>");
+    }
+
+    public void testMultipleLinks() {
+        String ticketUrl = "<a href='" + TRAC_URL + "ticket/101'>#101</a>";
+        String changesetUrl = "<a href='" + TRAC_URL + "changeset/303'>[303]</a>";
+        assertAnnotatedTextEquals("#101 for [303]: Text", ticketUrl + " for " + changesetUrl + ": Text");
+    }
+
     private void assertAnnotatedTextEquals(String originalText, String expectedAnnotatedText) {
         MarkupText markupText = new MarkupText(originalText);
 
