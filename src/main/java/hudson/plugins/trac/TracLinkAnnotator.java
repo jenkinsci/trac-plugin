c689a8c97dad25e79a3862362c7b73f2ba68c252
bradfritz <bradfritz@dev.java.net>
2009-02-02 03:47:46 +0000

    [HUDSON-2707] Remainder of Rick Riemer's patch.  Adds support for Trac InterTrac links(1).  The following formats are supported:
     * #T123
     * trac:#123
     * trac:ticket:123
    
     * [T123]
     * trac:changeset:123
    
     * trac:wiki:PageName
    
    (1) http://trac.edgewall.org/wiki/InterTrac

--- src/main/java/hudson/plugins/trac/TracLinkAnnotator.java
+++ src/main/java/hudson/plugins/trac/TracLinkAnnotator.java
@@ -10,6 +10,7 @@ import java.util.regex.Pattern;
 
 /**
  * Annotates <a href="http://trac.edgewall.org/wiki/TracLinks">TracLink</a>
+ * and <a href="http://trac.edgewall.org/wiki/InterTrac">InterTrac</a>
  * notation in changelog messages.
  *
  * @author Kohsuke Kawaguchi
@@ -56,7 +57,7 @@ public class TracLinkAnnotator extends ChangeLogAnnotator {
 
     private static final LinkMarkup[] MARKUPS = new LinkMarkup[] {
         new LinkMarkup(
-            "(?:#|ticket:)NUM",
+            "(?<!\\:)(?:#|ticket:)NUM",  // "#123" or "ticket:123" but not ":#123" or ":ticket:123"
             "ticket/$1"),
         new LinkMarkup(
             "comment:ticket:NUM:NUM",
@@ -65,19 +66,34 @@ public class TracLinkAnnotator extends ChangeLogAnnotator {
             "\\{NUM\\}|report:NUM",
             "report/$1$2"),  // only $1 or $2 matches, and the other will expand to ""
         new LinkMarkup(
-            "rNUM:NUM|\\[NUM:NUM\\]|log:@NUM:NUM",
+            "rNUM:NUM|\\[NUM:NUM\\]|(?<!\\:)log:@NUM:NUM",
             "log/?rev=$2$4$6&stop_ver=$1$3$5"),
         new LinkMarkup(
-            "rNUM(?!:)|\\[NUM\\]|changeset:NUM", // (?!:) is a position match with negative look ahead, so that "r5" portion of "r5:6" won't match.
+            "rNUM(?!:)|\\[NUM\\]|(?<!\\:)changeset:NUM", // (?!:) is a position match with negative look ahead, so that "r5" portion of "r5:6" won't match.
             "changeset/$1$2$3"),
         // TODO: log:trunk@1:3 format
         // TODO: diffs
         new LinkMarkup(
-            "((?:[A-Z][a-z]+){2,})|wiki:ANYWORD",
+            "(?<!\\:)(?:((?:[A-Z][a-z]+){2,})|wiki:ANYWORD)",
             "wiki/$1$2"),
         new LinkMarkup(
             "milestone:ANYWORD",
             "milestone/$1"),
         // TODO: attachment and file.
+        new LinkMarkup( // InterTrac ticket links (short: #T123)
+            "#([a-zA-Z])NUM",
+            "search?q=%23$1$2"),
+        new LinkMarkup( // InterTrac changeset links (short: [T123])
+             "\\[([a-zA-Z])NUM\\]",
+             "search?q=%5B$1$2%5D"),
+        new LinkMarkup( // InterTrac ticket links (medium: trac:#123)
+            "ANYWORD\\:#NUM",
+            "search?q=$1%3A%23$2"),
+        new LinkMarkup( // InterTrac ticket or changeset links (full: trac:ticket:123)
+            "ANYWORD\\:((?:ticket)|(?:changeset))\\:NUM",
+            "search?q=$1%3A$2%3A$3"),
+        new LinkMarkup( // InterTrac wiki links (full: trac:wiki:PageName)
+            "ANYWORD\\:wiki\\:ANYWORD",
+            "search?q=$1%3Awiki%3A$2")
     };
 }
